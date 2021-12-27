b3de9fd34da49f88f0d91a2d77239a65d4d4bc29
Daniel C. Landon Jr <mujun.kross@outlook.com>
2021-12-27 05:54:27 -0500

    Added comments. Started creating a route table.

--- server.js
+++ server.js
@@ -1,12 +1,44 @@
+// dependencies
 const http = require('http');
+
+// data
 const products = require('./data/products.json');
 
+// server object...what does the server do when it receives a request
 const server = http.createServer((request, response) => {
-    response.writeHead(200, {'Content-Type': 'application/json'})
-    response.write(JSON.stringify(products))
-    response.end();
+    // is the request a GET or a POST
+    switch(request.method)  
+    {
+        default: // not a GET or a POST
+            response.writeHead(404, {'Content-Type': 'application/json'});
+            response.end(JSON.stringify({ 'message': 'Pack Sand!' }));
+            break;
+
+        case 'GET':
+            switch (request.url) {
+                default:
+                    response.writeHead(404, {'Content-Type': 'application/json'});
+                    response.end(JSON.stringify({ 'message': 'Pack Sand!' }));
+                    break;
+
+                case '/products/api':
+                    response.writeHead(200, {'Content-Type': 'application/json'});
+                    response.end(JSON.stringify(products));
+                    break;
+            }
+            break;
+        
+        case 'POST':
+            response.writeHead(200, {'Content-Type': 'application/json'});
+            response.end(JSON.stringify( {'message': 'WIP!!!'} ));
+            break;
+    }
 });
 
+// use process.env.port or port 5000
 const PORT = process.env.PORT || 5000;
 
-server.listen(PORT, () => {console.log(`SERVER is running on port ${PORT}`)});
+// star the server, listen for a response
+server.listen(PORT, () => {
+    console.log(`SERVER is running on port ${PORT}`);
+});
